16f0ef1cffa82170c8666423e94339008295b9ed
kurosh saadati <ksutility@gmail.com>
2024-04-18 16:35:50 +0330

    130- add decorate func for cornometer 1 prossecc

--- modules/k_tools.py
+++ modules/k_tools.py
@@ -4,6 +4,11 @@ Created on 1402/12/17
     @author: ks
     last update 1402/12/17
 """
+debug=False
+from functools import wraps
+import time
+import k_err
+    
 #k_form
 def var_compare(x1,x2,None_to_str=True):
     '''
@@ -30,3 +35,31 @@ def var_inf(x):
         except:
             pass
     return {'type':type(x),'len':l,'str':s}
+def x_cornometer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        t1=time.time()
+        xxx=func(*args, **kwargs)
+        if debug:
+            t2=time.time()
+            try:
+                tt1=nth_item_of_dict(nth_item_of_dict(kwargs,1),1)
+            except:
+                tt1='*err* => nth_item_of_dict'
+            try:
+                tt2=",".join([kwargs[x] for x in kwargs if type(kwargs[x])==str])
+            except:
+                tt2='*err* '    
+                
+            dtm1="{:.4f}".format(t2-t1) #delta time 1
+            tm1='{:.4f}'.format(t1 % 1000) #time 1
+            tm2='{:.4f}'.format(t2 % 1000) #time 2
+            #k_err.xxxprint(msg=[dtm1,tm1,tm2],vals=kwargs)
+            t3=time.time()
+            tm3='{:.4f}'.format(t3 % 1000) #time 3
+            dtm2='{:.4f}'.format(t3-t2) #delta time 2
+            print('--------{} , {} : {} - {} , func = {} , {} , args = {}'.format(dtm1,dtm2,tm1,tm3,func.__name__,tt1,tt2))
+        return xxx
+    return wrapper
+def nth_item_of_dict(xdic,n):
+    return next(iter(xdic.items()))[n]
