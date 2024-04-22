be6872fb46b5ea5b74833b4389ce2c0257805f23
kurosh saadati <ksutility@gmail.com>
2024-04-22 16:13:39 +0330

    202-refine temp print data

--- modules/k_user.py
+++ modules/k_user.py
@@ -107,5 +107,5 @@ def auth(auth_jobs):
     from gluon import current
     session=current.session
     res=(session["admin"] or user_in_jobs(session["username"],auth_jobs,{}))
-    print (f'session["admin"]={session["admin"]},auth_jobs={auth_jobs},res={res}')
+    print (f'un={session["username"]},session["admin"]={session["admin"]},auth_jobs={auth_jobs},res={res}')
     return res
