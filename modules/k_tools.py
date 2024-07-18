f163883cbb0f99b84f7548be26da7601122f8fd2
kurosh saadati <ksutility@gmail.com>
2024-07-18 16:42:39 +0330

    428-fix bugs

--- modules/k_tools.py
+++ modules/k_tools.py
@@ -19,12 +19,12 @@ def var_compare(x1,x2,None_to_str=True):
     if None_to_str:
         if x1==None:x1=''
         if x2==None:x2=''
+    y1=var_inf(x1)
+    y2=var_inf(x2)    
     if x1==x2:
-        return {'ok':True,'msg':' = : ' + str(x1)}
+        return {'ok':True,'msg': f"({y1['str']} = {y2['str']})"}
     else:
-        y1=var_inf(x1)
-        y2=var_inf(x2)
-        return {'ok':False,'msg':f" ! : {y1['type']}({y1['len']}),{y2['type']}({y2['len']}) -- {y1['str']},{y2['str']}"}
+        return {'ok':False,'msg':f"({y1['str']} != {y2['str']}) : {y1['type']}({y1['len']}) != {y2['type']}({y2['len']}) "}
 def var_inf(x):
     l=''
     s=''
